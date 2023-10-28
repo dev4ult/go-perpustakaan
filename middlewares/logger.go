863c9250fcb459ca4e9548b28da41b05304399f4
dev4ult <nibrasbiasaaja@gmail.com>
2023-10-28 07:08:00 +0700

    feat: logger middleware with logrus

--- middlewares/logger.go
+++ middlewares/logger.go
@@ -1,1 +1,24 @@
 package middlewares
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/sirupsen/logrus"
+)
+
+var log = logrus.New()
+
+func Logger() echo.MiddlewareFunc {
+	return middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:    true,
+		LogStatus: true,
+		LogValuesFunc: func(ctx echo.Context, values middleware.RequestLoggerValues) error {
+			log.WithFields(logrus.Fields{
+				"URI":   values.URI,
+				"status": values.Status,
+			}).Info("request")
+	
+			return nil
+		},
+	})
+}
