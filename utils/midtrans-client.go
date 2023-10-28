6a40b18e3a40bb2a2c4e62d03bbbbecf4fe344d2
dev4ult <nibrasbiasaaja@gmail.com>
2023-10-28 07:09:51 +0700

    feat: core api client

--- utils/midtrans-client.go
+++ utils/midtrans-client.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"github.com/midtrans/midtrans-go"
+	"github.com/midtrans/midtrans-go/coreapi"
 	"github.com/midtrans/midtrans-go/snap"
 )
 
@@ -9,4 +10,10 @@ func SnapClient(midtransServerKey string) snap.Client {
 	var snapClient snap.Client
 	snapClient.New(midtransServerKey, midtrans.Sandbox)
 	return snapClient
+}
+
+func CoreAPIClient(midtransServerKey string) coreapi.Client {
+	var coreAPIClient coreapi.Client
+	coreAPIClient.New(midtransServerKey, midtrans.Sandbox)
+	return coreAPIClient
 }
