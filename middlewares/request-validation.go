681cc38c18b5aa8d73369e4643ab0978865ca0ac
dev4ult <nibrasbiasaaja@gmail.com>
2023-11-03 16:38:36 +0700

    fix: set a new variable for a new request so it will not save the previous data

--- middlewares/request-validation.go
+++ middlewares/request-validation.go
@@ -2,6 +2,7 @@ package middlewares
 
 import (
 	"perpustakaan/helpers"
+	"reflect"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
@@ -10,14 +11,17 @@ import (
 func RequestValidation(request any) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(ctx echo.Context) error {
-			if err := ctx.Bind(request); err != nil {
+			var requestType = reflect.TypeOf(request)
+			var newRequest = reflect.New(requestType).Interface()
+
+			if err := ctx.Bind(newRequest); err != nil {
 				errorMap := helpers.ParseError(err.Error())
 				return ctx.JSON(errorMap["code"].(int), helpers.Response(errorMap["message"].(string)))
 			}
 
 			var validate = validator.New()
 
-			if err := validate.Struct(request); err != nil {
+			if err := validate.Struct(newRequest); err != nil {
 				var errMap = []map[string]string{} 
 				for _, err := range err.(validator.ValidationErrors) {
 					errMap = append(errMap, map[string]string {
@@ -31,7 +35,7 @@ func RequestValidation(request any) echo.MiddlewareFunc {
 				}))
 			}
 
-			ctx.Set("request", request)
+			ctx.Set("request", newRequest)
 
 			return next(ctx)
 		}
