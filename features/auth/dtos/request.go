1596b5922f1e4d6baa6ef06ccc3f1d2672c47178
dev4ult <nibrasbiasaaja@gmail.com>
2023-11-03 16:37:58 +0700

    fix: change to snake case

--- features/auth/dtos/request.go
+++ features/auth/dtos/request.go
@@ -1,21 +1,21 @@
 package dtos
 
 type InputLogin struct {
-	CredentialNumber string `json:"credential-number" form:"credential-number"`
-	StaffID string `json:"staff-id" form:"staff-id"`
+	CredentialNumber string `json:"credential_number" form:"credential_number"`
+	StaffID string `json:"staff_id" form:"staff_id"`
 	Password string `json:"password" form:"password" validate:"required"`
 }
 
 type InputStaffRegistration struct {
-	FullName string `json:"full-name" form:"full-name" validate:"required"`
-	StaffID string `json:"staff-id" form:"staff-id" validate:"required"`
+	FullName string `json:"full_name" form:"full_name" validate:"required"`
+	StaffID string `json:"staff_id" form:"staff_id" validate:"required"`
 	NIK int `json:"nik" form:"nik" validate:"required"`
-	PhoneNumber string `json:"phone-number" form:"phone-number" validate:"required"`
+	PhoneNumber string `json:"phone_number" form:"phone_number" validate:"required"`
 	Address string `json:"address" form:"address" validate:"required"`
 	Email string `json:"email" form:"email" validate:"required"`
 	Password string `json:"password" form:"password" validate:"required"`
 }
 
 type Authorization struct {
-	AccessToken string `json:"access-token" form:"access-token"`
+	AccessToken string `json:"access_token" form:"access_token"`
 }
--- features/auth/dtos/response.go
+++ features/auth/dtos/response.go
@@ -1,23 +1,23 @@
 package dtos
 
 type ResAuthorization struct {
-	FullName string `json:"full-name"`
-	AccessToken string `json:"access-token"`
-	RefreshToken string `json:"refresh-token"`
+	FullName string `json:"full_name"`
+	AccessToken string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
 }
 
 type ResLibrarian struct {
-	FullName string `json:"full-name"`
-	StaffID string `json:"staff-id"`
+	FullName string `json:"full_name"`
+	StaffID string `json:"staff_id"`
 	NIK int `json:"nik"`
-	PhoneNumber string `json:"phone-number"`
+	PhoneNumber string `json:"phone_number"`
 	Address string `json:"address"`
 	Email string `json:"email"`
 }
 
 type Token struct {
-	AccessToken string `json:"access-token"`
-	RefreshToken string `json:"refresh-token"`
+	AccessToken string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
 }
 
 type User struct {
