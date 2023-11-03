c6ac770dc63a03f5377af697873dc77b0d8b4c9f
dev4ult <nibrasbiasaaja@gmail.com>
2023-11-03 16:39:46 +0700

    fix: remove & to pass by value

--- routes/auth.go
+++ routes/auth.go
@@ -9,7 +9,7 @@ import (
 )
 
 func Auths(e *echo.Echo, handler auth.Handler) {
-	e.POST("/login", handler.Login(), m.RequestValidation(&dtos.InputLogin{}))
+	e.POST("/login", handler.Login(), m.RequestValidation(dtos.InputLogin{}))
 	e.POST("/refresh", handler.Refresh(), m.Authorization("all", true), m.RequestValidation(&dtos.Authorization{}))
-	e.POST("/staff", handler.StaffRegistration(), m.RequestValidation(&dtos.InputStaffRegistration{}))
+	e.POST("/staff", handler.StaffRegistration(), m.RequestValidation(dtos.InputStaffRegistration{}))
 }
